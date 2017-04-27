aebcd3695313185c3349f6f8e27222a61fa7c856
sasakazu <dropout.college2014@gmail.com>
2017-04-27 14:35:44 +0900

    group new params done.........

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,39 @@
 class GroupsController < ApplicationController
+
   def new
+    @group = Group.new
   end
 
   def show
+    @group = Group.find(params[:id])
+
   end
 
   def edit
   end
+
+  def create
+    @group = current_user.groups.build(group_params)
+
+    if @group.save
+      redirect_to @group
+
+    else
+      render "new"
+    end
+
+  end
+
+
+
+  private
+
+      def group_params
+                params.require(:group).permit(:name, :icon)
+      end
+
+
+
+
+
 end
